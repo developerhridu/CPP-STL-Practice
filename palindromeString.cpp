ba365995b7ebf3fa8bdd40febe45746edc826afd
developerhridu <72350795+developerhridu@users.noreply.github.com>
2021-02-25 15:57:57 +0600

    Fast I/O in CPP

--- palindromeString.cpp
+++ palindromeString.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 
 void palindromeOrNot()
 {
